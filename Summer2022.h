1f1ca7af4c51691a0c5d81d07616067162f8fb60
Stanley Fung <stanley_fung@Stanleys-MacBook-Air.local>
2022-09-22 11:49:37 -0400

    Header File

---
+++ Summer2022.h
@@ -0,0 +1,26 @@
+//
+//  Summer2022.hpp
+//  Summer2022
+//
+//  Created by Stanley Fung on 7/5/22.
+//
+
+#ifndef Summer2022_h
+#define Summer2022_h
+
+#include <stdio.h>
+#include <vector>
+#include <iostream>
+
+
+class Bank{
+    public:
+    Bank();
+
+    ~Bank();
+    private:
+    float account_balance;
+    int account number;
+};
+
+#endif /* Summer2022_hpp */
